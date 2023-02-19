8e1e208fb9ed17d604c96512aa907e01f9a08d7b
Abhishek Pandya <aspandya30398@gmail.com>
2023-02-18 19:43:57 -0600

    Fetching data from GitHub API

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,32 +2,10 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "abhi.pandya",
-        avatar_url: "https://avatars.githubusercontent.com/u/22200616?v=4",
-        html_url: "https://github.com/Abhishek3098",
-      },
-      {
-        id: "2",
-        login: "abhi.pandya",
-        avatar_url: "https://avatars.githubusercontent.com/u/22200616?v=4",
-        html_url: "https://github.com/Abhishek3098",
-      },
-      {
-        id: "3",
-        login: "abhi.pandya",
-        avatar_url: "https://avatars.githubusercontent.com/u/22200616?v=4",
-        html_url: "https://github.com/Abhishek3098",
-      },
-    ],
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
